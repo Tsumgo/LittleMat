b3d19485fefbe0d82bfcdf0811c22727061c5609
Tsum <2207428258@qq.com>
2023-01-07 19:16:26 +0800

    非动态空间版本

--- test.cpp
+++ test.cpp
@@ -1,31 +1,36 @@
 #include <cstdio>
+#include <cstring>
+#include <cstdlib>
 using namespace std;
 class Node
 {
-private:
-    int r=1,c=2;
-    int arr[10];
 public:
-    Node(int a, int b);
-    ~Node();
-    // int foo(int x)
-    // {
-    //     int arr[x];
-    //     arr[0]=1;
-    // }
+    int r,c;
+    int *arr;
+public:
+    Node(int a, int b): r(a), c(b)
+    {
+        arr = (int*) calloc (a*b, sizeof (int));
+    }
+    ~Node()
+    {
+        printf("%d:", arr);
+        free(arr); 
+        puts("Freed!");
+    }
+    friend Node ttt();
 };
 
-Node::Node(int a, int b): r(a), c(b)
+Node ttt()
 {
+    Node ret(2,2);
+    for (int i=0;i<2;i++)
+        for (int j=0;j<2;j++) scanf("%d",&ret.arr[i*2 + j]);
+    return ret;
 }
-
-Node::~Node()
-{
-}
-
 int main()
 {
-    Node X(10, 10);
-    printf("%d\n", sizeof (Node));
+    Node me = ttt();
+    // printf("%d\n",me.arr);
     return 0;
 }
